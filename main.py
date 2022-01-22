fa1244ce2a24bc38383266d33a1d5e00e86d998d
djsamyo <soumyo.sathya@gmail.com>
2022-01-22 20:06:48 +0400

    Created volunteer form

---
+++ main.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import ttk
+global appName
+import pandas as pd
+appName = "Di@Tech-"
+class volunteerForm:
+    def __init__(self, root):
+
+        root.title(appName+"New Volunteer Sign Up")
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(2, weight=1)
+        root.rowconfigure(5, weight=1)
+
+        self.name=StringVar()
+        ttk.Label(mainframe, text="Name :").grid(column=0, row=0)
+        name_entry = ttk.Entry(mainframe, width=14, textvariable=self.name)
+        name_entry.grid(column=1, row=0)
+
+        self.email=StringVar()
+        ttk.Label(mainframe, text="Email :").grid(column=0, row=1, sticky=E)
+        email_entry = ttk.Entry(mainframe, width=14,textvariable=self.email)
+        email_entry.grid(column=1, row=1)
+
+        self.year=StringVar()
+        ttk.Label(mainframe, text="Year :").grid(column=0, row=2, sticky=E)
+        year_entry = ttk.Entry(mainframe, width=14,textvariable=self.year)
+        year_entry.grid(column=1, row=2)
+
+        ttk.Button(mainframe, text="Register",command=self.registerVolunteer).grid(column=3, row=3, sticky=W)
+
+
+        for child in mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+    def registerVolunteer(self,*args):
+        volunteer_data = pd.read_csv('data/volunteers.csv')
+        name=self.name.get()
+        email=self.email.get()
+        year=int(self.year.get())
+        print(volunteer_data)
+        volunteer_data.loc[len(volunteer_data.index)] = [name,email,year]
+        print(volunteer_data)
+        volunteer_data.to_csv('data/volunteers.csv',index=False)
+
+
+
+root = Tk()
+volunteerForm(root)
+root.mainloop()
