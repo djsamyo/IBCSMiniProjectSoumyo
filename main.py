32fd3807df02cb764314847d5d7cde963a43e922
djsamyo <soumyo.sathya@gmail.com>
2022-01-25 08:17:46 +0400

    reformated files

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+
 global appName
 from volunteer import volunteerForm
 import pandas as pd
 
+
 class mainFrame():
-    def __init__(self,parent):
+    def __init__(self, parent):
         global mainframe
         mainframe = ttk.Frame(parent, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -16,19 +18,21 @@ class mainFrame():
         menubar = Menu(root)
 
         registerMenu = Menu(menubar, tearoff=0)
-        registerMenu.add_command(label="Volunteers",command=self.openVolunteerFrame)
+        registerMenu.add_command(label="Volunteers", command=self.openVolunteerFrame)
         registerMenu.add_command(label="Supervisors")
         menubar.add_cascade(label="Register", menu=registerMenu)
 
         parent.config(menu=menubar)
+
     def openVolunteerFrame(self):
         for widget in mainframe.winfo_children():
             widget.destroy()
         volunteerForm(mainframe)
+
+
 appName = "Di@Tech"
 root = Tk()
 root.title(appName)
 mainFrame(root)
 
 root.mainloop()
-
--- supervisors.py
+++ supervisors.py
@@ -1,42 +1,45 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+
 global appName
 import pandas as pd
+
+
 class supervisorForm:
     def __init__(self, parent):
-
 
         mainframe = ttk.Frame(parent, padding="3 3 12 12")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         parent.columnconfigure(2, weight=1)
         parent.rowconfigure(5, weight=1)
 
-        self.name=StringVar()
+        self.name = StringVar()
         ttk.Label(mainframe, text="Name :").grid(column=0, row=0)
         name_entry = ttk.Entry(mainframe, width=30, textvariable=self.name)
         name_entry.grid(column=1, row=0, columnspan=3)
 
-        self.email=StringVar()
+        self.email = StringVar()
         ttk.Label(mainframe, text="Email :").grid(column=0, row=1, sticky=E)
-        email_entry = ttk.Entry(mainframe, width=30,textvariable=self.email)
+        email_entry = ttk.Entry(mainframe, width=30, textvariable=self.email)
         email_entry.grid(column=1, row=1, columnspan=3)
 
-        self.year=StringVar()
+        self.year = StringVar()
         ttk.Label(mainframe, text="Year :").grid(column=0, row=2, sticky=E)
-        year_entry = ttk.Entry(mainframe, width=30,textvariable=self.year)
-        year_entry.grid(column=1, row=2,  columnspan=3)
+        year_entry = ttk.Entry(mainframe, width=30, textvariable=self.year)
+        year_entry.grid(column=1, row=2, columnspan=3)
 
-        ttk.Button(mainframe, text="Register",command=self.registerVolunteer).grid(column=3, row=3, sticky=W)
+        ttk.Button(mainframe, text="Register", command=self.registerVolunteer).grid(column=3, row=3, sticky=W)
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
-    def registerVolunteer(self,*args):
+
+    def registerVolunteer(self, *args):
         volunteer_data = pd.read_csv('data/volunteers.csv')
         if self.name.get() and self.email.get():
-            name=self.name.get()
-            email=self.email.get()
+            name = self.name.get()
+            email = self.email.get()
             try:
-                year=int(self.year.get())
+                year = int(self.year.get())
                 print(volunteer_data)
                 volunteer_data.loc[len(volunteer_data.index)] = [name, email, year]
                 print(volunteer_data)
